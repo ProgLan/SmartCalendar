16e4a4c8aff2445058194c96d9207a5d34b6a850
proglan <lz376@cornell.edu>
2015-11-04 19:30:52 -0500

    update file structure and enums group

---
+++ SCCalendarViewPresentationMode.swift
@@ -0,0 +1,19 @@
+//
+//  SCCalendarViewPresentationMode.swift
+//  SmartCalendar
+//
+//  Created by Lan Zhang on 11/4/15.
+//  Copyright © 2015 Lan Zhang. All rights reserved.
+//
+
+import UIKit
+
+@objc public enum SCCalendarViewPresentationMode: Int {
+    case MonthView
+    case WeekView
+    case DayView
+    
+    case ListView
+    case EventDisplayView
+    case modelView
+}
